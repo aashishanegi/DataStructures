5aa65079ce2aeeb7fe2c6abf72ee992fb2b21b31
Aashisha Negi <500091664@stu.upes.ac.in>
2024-06-15 18:49:52 +0530

    Gfg Leaders in an array

---
+++ Day3_10.cpp
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+// C++ program to remove recurring digits from
+// a given number
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+
+class Solution {
+    // Function to find the leaders in the array.
+  public:
+    vector<int> leaders(int n, int arr[]) {
+        // Code here
+       vector<int> ans;
+  
+    for (int i = 0; i < n; i++) {
+    bool leader = true;
+        for (int j = i + 1; j < n; j++)
+            if (arr[j] > arr[i]) {
+                leader = false;
+                break;
+      }
+    if (leader)
+    ans.push_back(arr[i]);
+  }
+  
+  return ans;
+}
+
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    long long t;
+    cin >> t; // testcases
+    while (t--) {
+        long long n;
+        cin >> n; // total size of array
+
+        int arr[n];
+
+        // inserting elements in the array
+        for (long long i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution obj;
+        // calling leaders() function
+        vector<int> v = obj.leaders(n, arr);
+
+        // printing elements of the vector
+        for (auto it = v.begin(); it != v.end(); it++) {
+            cout << *it << " ";
+        }
+
+        cout << endl;
+    }
+}
+
+// } Driver Code Ends
