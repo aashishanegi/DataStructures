cbe78e43bec025f9a26bc076e4bef0645c073f80
Aashisha Negi <500091664@stu.upes.ac.in>
2024-06-15 10:22:26 +0530

    Leetcode 75 - Sort Colors

---
+++ Day3_3.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int c0=0, c1=0, c2=0;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            if(nums[i]==0) c0++;
+            else if (nums[i]==1) c1++;
+            else c2++;
+        }
+        for(int i=0;i<c0;i++)
+        {
+            nums[i]=0;
+        }
+        for(int i=c0;i<c0+c1;i++)
+        {
+            nums[i]=1;
+        }
+        for(int i=c1+c0;i<n;i++)
+        {
+            nums[i]=2;
+        }
+
+}
+    
+};
