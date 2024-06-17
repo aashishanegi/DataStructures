de0db610650483db581c5336044ec7f0b9c43479
Aashisha Negi <500091664@stu.upes.ac.in>
2024-06-17 19:01:17 +0530

    Leetcode 118 - Pascals triamgle

--- Day5_4.cpp
+++ Day5_4.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
-        vector <vector<int>> pascal(numRows);
+        vector <vector<int>> pascals(numRows);
         for(int i=0;i<numRows;i++)
         {
-            pascal[i].resize(i+1);
+            pascals[i].resize(i+1);
             for(int j=0;j<=i;j++)
             {
                 if(j==0 || j==i)
                 {
-                    pascal[i][j]=1;
+                    pascals[i][j]=1;
                 }
                 else
                 {
-                    pascal[i][j]=pascal[i-1][j]+pascal[i-1][j-1];
+                    pascals[i][j]=pascals[i-1][j]+pascals[i-1][j-1];
                 }
             }
         }
-        return pascal;
+        return pascals;
         
     }
 };
