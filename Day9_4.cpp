63f52a2916c18ae098882ece5df623b85ed3dac6
Aashisha Negi <500091664@stu.upes.ac.in>
2024-06-21 17:27:50 +0530

    Leetcode 74 - Search a 2D Matrix

---
+++ Day9_4.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int rows=matrix.size();
+        int col=matrix[0].size();
+        for(int i=0;i<rows;i++)
+        {
+            for(int j=0;j<col;j++)
+            {
+                if(matrix[i][j]==target)
+                {
+                    return true;
+                    break;
+                }
+            }
+        }
+        return false;
+    }
+};
