58d2de56b9cfba53afde58050d3c27a62becd40b
Aashisha Negi <500091664@stu.upes.ac.in>
2024-06-21 21:59:39 +0530

    Merge Sort

---
+++ Day9_6.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    void merge(vector<int>& nums, int low, int mid,int high)
+    {
+        vector<int> temp;
+        int left=low;
+        int right=mid+1;
+        while(left<=mid && right<=high)
+        {
+            if(nums[left]<=nums[right])
+            {
+                temp.push_back(nums[left]);
+                left++;
+            }
+            else{
+                temp.push_back(nums[right]);
+                right++;
+            }
+        }
+        while(left<=mid)
+        {
+            temp.push_back(nums[left]);
+            left++;
+        }
+        while(right<=high)
+        {
+            temp.push_back(nums[right]);
+            right++;
+        }
+        for (int i = low; i <= high; i++) {
+        nums[i] = temp[i - low];
+    }
+    }
+    void mergesort(vector<int>& nums, int low,int high)
+    {
+        if(low>=high) return;
+        int mid=(low+high)/2;
+        mergesort(nums,low,mid);
+        mergesort(nums,mid+1,high);
+        merge(nums,low,mid,high);
+    }
+    vector<int> sortArray(vector<int>& nums) {
+        int n=nums.size();
+        mergesort(nums,0,n-1);
+        return nums;
+    }
+};
