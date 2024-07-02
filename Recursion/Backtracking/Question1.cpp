674498e3c65254e036c77e09a34cea18095c64a7
Aashisha Negi <500091664@stu.upes.ac.in>
2024-07-02 11:59:59 +0530

    Leetcode 46 - Permutations

---
+++ Recursion/Backtracking/Question1.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void find(int idx, vector<int> &nums, vector<vector<int>> &ans)
+    {
+        if(idx==nums.size())
+        {
+            ans.push_back(nums);
+            return;
+        }
+        for(int i=idx;i<nums.size();i++)
+        {
+            swap(nums[idx],nums[i]);
+            find(idx+1,nums,ans);
+            swap(nums[idx],nums[i]);
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>>ans;
+        find(0,nums,ans);
+        return ans;       
+    }
+};
