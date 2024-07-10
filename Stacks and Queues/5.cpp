e6333ced5adc5a1194348b130ed13adbdf2ffde7
Aashisha Negi <500091664@stu.upes.ac.in>
2024-07-10 11:28:46 +0530

    Leetcode 20 - Valid Paranthesis

---
+++ Stacks and Queues/5.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack <int> st;
+        for(auto it: s) {
+        if(it=='(' || it=='[' || it== '{')
+        st.push(it);
+        else
+        {
+            if(st.empty()) return false; 
+            char ch = st.top(); 
+            st.pop(); 
+            if((it == ')' and ch == '(') or  (it == ']' and ch == '[') or (it == '}' and ch == '{')) continue;
+            else return false;
+        }
+        }
+        return st.empty();
+    }
+};
